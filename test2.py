643318d44cd91155c7a6b92a591d7fdab51a245b
Timothy Bruce <timothy.bruce@maine.edu>
2022-04-14 19:26:50 -0400

    Update test2.py
    Fixed memory problem for larger files

--- test2.py
+++ test2.py
@@ -1,6 +1,3 @@
-
-import os
-
 import cv2
 import numpy as np
 from vidstab import VidStab, download_ostrich_video, layer_overlay
@@ -12,7 +9,7 @@ from vidstab import VidStab, download_ostrich_video, layer_overlay
 # Initialize object tracker, stabilizer, and video reader
 object_tracker = cv2.TrackerCSRT_create()
 stabilizer = VidStab()
-vidcap = cv2.VideoCapture("data\\15_meters.mov")
+vidcap = cv2.VideoCapture("data\\15_meter.MOV")
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 out = False
 
@@ -22,19 +19,23 @@ object_bounding_box = None
 box_dim = [0,0]
 box = [0, 0, 4000, 2250]
 
+f = 0
+
 while True:
+    f = f + 1
+    print(f)
     grabbed_frame, frame = vidcap.read()
 
     #frame = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
 
     # Pass frame to stabilizer even if frame is None
-    stabilized_frame = stabilizer.stabilize_frame(input_frame=frame, border_size=0)
+    stabilized_frame = stabilizer.stabilize_frame(input_frame=frame, smoothing_window=20, border_size=0)
+
+    frame = False
 
     # If stabilized_frame is None then there are no frames left to process
     if stabilized_frame is None:
         break
-
-    T = np.float32([[1, 0, 0], [0, 1, 0]])
 
     # Draw rectangle around tracked object if tracking has started
     if object_bounding_box is not None:
@@ -65,7 +66,8 @@ while True:
             #print(box[2], box[3])
             #cv2.rectangle(stabilized_frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (255, 0, 0), 2)
             if not out:
-                out = cv2.VideoWriter('output.mp4',fourcc, 25, (box[2],box[3]))
+                fps = vidcap.get(cv2.CAP_PROP_FPS)
+                out = cv2.VideoWriter('output.mp4',fourcc, fps, (box[2],box[3]))
             #stabilized_frame = cv2.resize(stabilized_frame,(box[2],box[3]),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
             out.write(stabilized_frame)
 
